51ff82823936a8fb92792ae9424067554bcd4bd6
vitorp <vitormax2005@hotmail.com>
2024-10-21 13:00:35 -0300

    Problem #27

---
+++ LeetCode/RemoveElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    partial class Solution
+    {
+        // Problem RemoveElement #27
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            int len = nums.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+
+            }
+
+            return k;
+        }
+    }
+}
