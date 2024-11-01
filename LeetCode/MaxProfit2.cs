d6c54d82c0161476bcc00077c7badbd91d932ac4
vitorp <vitormax2005@hotmail.com>
2024-11-01 11:37:43 -0300

    Problem #55

---
+++ LeetCode/MaxProfit2.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+partial class Solution
+{
+    // Problem MaxProfit2 #122
+    public int MaxProfit2(int[] prices)
+    {
+        int min = prices[0];
+        int totalProfit = 0;
+
+        foreach (int price in prices)
+        {
+            if (price < min) min = price;
+            totalProfit = Math.Max(totalProfit, price - min);
+        }
+        return totalProfit;
+    }
+}
